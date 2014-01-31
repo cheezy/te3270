111a2a2a8a4a3729c03072a6ccd5f31e12200194
cheezy <jeff.morgan@leandog.com>
2014-01-31 05:45:39 -0500

    fixed up the comments a little

--- lib/te3270/emulators/extra.rb
+++ lib/te3270/emulators/extra.rb
@@ -28,62 +28,84 @@ module TE3270
         @screen = session.Screen
         @area = screen.SelectAll
       end
+
+      #
       # Disconnects the Extra System connection
+      #
       def disconnect
         system.Quit
       end
+
+      #
       # Creates a method to extract text of specified length from a start point on the extra Screen object.
-      # @param [int] the x coordinate of location on the screen.
-      # @param [int] the y coordinate of location on the screen.
-      # @param [int] the length of string to extract
+      # @param [Fixnum] the x coordinate of location on the screen.
+      # @param [Fixnum] the y coordinate of location on the screen.
+      # @param [Fixnum] the length of string to extract
       # @return [String]
+      #
       def get_string(row, column, length)
         screen.GetString(row, column, length)
       end
 
+      #
       # Creates a method to put string at the coordinates specified on the extra Screen object.
       # Once the string is input, quiet period will ensure to not send data for a specified number of milliseconds
-      # @param [int] the string to set
-      # @param [int] the x coordinate of the location on the screen.
-      # @param [int] the y coordinate of the location on the screen.
+      # @param [String] the string to set
+      # @param [Fixnum] the x coordinate of the location on the screen.
+      # @param [Fixnum] the y coordinate of the location on the screen.
+      #
       def put_string(str, row, column)
         screen.PutString(str, row, column)
         quiet_period
       end
 
+      #
       # Creates a method to send keys to the screen. The keys are defined in function keys
-      # @param [KEYS] the function keys defined by Extra System
+      # @param [String] the function keys defined by Extra System
       # Once the string is input, quiet period will ensure to not send data for a specified number of milliseconds
+      #
       def send_keys(keys)
         screen.SendKeys(keys)
         quiet_period
       end
+
+      #
       # Creates a method to wait for the string to appear at the location
       #@param [String] the string to wait for
-      # @param [int] the x coordinate of location
-      # @param [int] the y coordinate of location
+      # @param [Fixnum] the x coordinate of location
+      # @param [Fixnum] the y coordinate of location
+      #
       def wait_for_string(str, row, column)
         wait_for do
           screen.WaitForString(str, row, column)
         end
       end
+
+      #
       # Waits for the host to not send data for a specified number of seconds
-      # @param [int] the number of seconds
+      # @param [Fixnum] the number of seconds
+      #
       def wait_for_host(seconds)
         wait_for(seconds) do
           screen.WaitHostQuiet
         end
       end
+
+      #
       # Waits until the cursor is at the specified location.
-      # @param [int] the x coordinate of the location
-      # @param [int] the y coordinate of the location
+      # @param [Fixnum] the x coordinate of the location
+      # @param [Fixnum] the y coordinate of the location
+      #
       def wait_until_cursor_at(row, column)
         wait_for do
           screen.WaitForCursor(row, column)
         end
       end
+
+      #
       # Creates a method to take screenshot of the active screen
       # @param [String] the path and name of the screenshot file to be saved
+      #
       def screenshot(filename)
         File.delete(filename) if File.exists?(filename)
         session.Visible = true unless visible
@@ -92,8 +114,10 @@ module TE3270
         session.Visible = false unless visible
       end
 
+      #
       # Creates method to return the text of the active screen
       # @return [String]
+      #
       def text
         area.Value
       end
