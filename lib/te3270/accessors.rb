784d5f8f807f7b4daf767a18348f3bf1631e22e6
machzqcq <macharla.pradeep.kumar@gmail.com>
2014-02-06 18:15:28 -0500

    Documentation for accessors added

--- lib/te3270/accessors.rb
+++ lib/te3270/accessors.rb
@@ -2,7 +2,22 @@
 module TE3270
   module Accessors
 
-    def text_field(name, row, column, length, editable=true)
+    #
+    # adds two methods to the screen object - one to set text in a text field,
+    # another to retrieve text from a text field.
+    #
+    # @example
+    #   text_field(:first_name, 23,45,20)
+    #   # will generate 'first_name', 'first_name=' method
+    #
+    # @param  [String] the name used for the generated methods
+    # @param [FixedNum] row number of the location
+    # @param [FixedNum] column number of the location
+    # @param [FixedNum] length of the text field
+    # @param [true|false] editable is by default true
+    #
+
+  def text_field(name, row, column, length, editable=true)
       define_method(name) do
         platform.get_string(row, column, length)
       end
