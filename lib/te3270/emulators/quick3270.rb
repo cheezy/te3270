7949d034f82e44396d6f3487265b144c06abb500
bhc480 <pradeep.macharla@capitalone.com>
2014-02-05 19:58:23 -0500

    Documentation for quick3270 methods

--- lib/te3270/emulators/quick3270.rb
+++ lib/te3270/emulators/quick3270.rb
@@ -3,9 +3,31 @@ require 'win32/screenshot'
 
 module TE3270
   module Emulators
+
+    #
+    # This class has the code necessary to communicate with the terminal emulator called Quick3270.
+    # You can use this emulator by providing the +:quick+ parameter to the constructor of your screen
+    # object or by passing the same value to the +emulator_for+ method on the +TE3270+ module.
+    #
+
     class Quick3270
 
       attr_writer :session_file, :visible, :max_wait_time
+
+      #
+      # Creates a method to connect to Quick System. This method expects a block in which certain
+      # platform specific values can be set.  Quick can take the following parameters.
+      #
+      # * session_file - this value is required and should be the filename of the session.
+      # * visible - determines if the emulator is visible or not. If not set it will default to +true+.
+      #
+      # @example Example calling screen object constructor with a block
+      #   screen_object = MyScreenObject.new(:quick)
+      #   screen_object.connect do |emulator|
+      #     emulator.session_file = 'path_to_session_file'
+      #     emulator.visible = true
+      #   end
+      #
 
       def connect
         start_quick_system
@@ -14,14 +36,35 @@ module TE3270
         establish_session
       end
 
+      #
+      # Disconnects the Quick System connection
+      #
+
       def disconnect
         session.Disconnect
         system.Application.Quit
       end
 
+      #
+      # Extracts text of specified length from a start point.
+      #
+      # @param [Fixnum] row the x coordinate of location on the screen.
+      # @param [Fixnum] column the y coordinate of location on the screen.
+      # @param [Fixnum] length the length of string to extract
+      # @return [String]
+      #
+
       def get_string(row, column, length)
         screen.GetString(row, column, length)
       end
+
+      #
+      # Puts string at the coordinates specified.
+      #
+      # @param [String] str the string to set
+      # @param [Fixnum] row the x coordinate of the location on the screen.
+      # @param [Fixnum] column the y coordinate of the location on the screen.
+      #
 
       def put_string(str, row, column)
         screen.MoveTo(row, column)
@@ -29,22 +72,57 @@ module TE3270
         quiet_period
       end
 
+      #
+      # Sends keystrokes to the host, including function keys.
+      #
+      # @param [String] keys keystokes up to 255 in length
+      #
+
       def send_keys(keys)
         screen.SendKeys(keys)
         quiet_period
       end
 
+      #
+      # Wait for the string to appear at the specified location
+      #
+      # @param [String] str the string to wait for
+      # @param [Fixnum] row the x coordinate of location
+      # @param [Fixnum] column the y coordinate of location
+      #
+
       def wait_for_string(str, row, column)
         screen.WaitForString(str, row, column)
       end
+
+      #
+      # Waits for the host to not send data for a specified number of seconds
+      #
+      # @param [Fixnum] seconds the maximum number of seconds to wait
+      #
 
       def wait_for_host(seconds)
         screen.WaitHostQuiet(seconds * 1000)
       end
 
+      #
+      # Waits until the cursor is at the specified location.
+      #
+      # @param [Fixnum] row the x coordinate of the location
+      # @param [Fixnum] column the y coordinate of the location
+      #
+
       def wait_until_cursor_at(row, column)
         screen.WaitForCursor(row, column)
       end
+
+      #
+      # Creates a method to take screenshot of the active screen.  If you have set the +:visible+
+      # property to false it will be made visible prior to taking the screenshot and then changed
+      # to invisible after.
+      #
+      # @param [String] filename the path and name of the screenshot file to be saved
+      #
 
       def screenshot(filename)
         File.delete(filename) if File.exists?(filename)
@@ -53,6 +131,12 @@ module TE3270
         Win32::Screenshot::Take.of(:window, title: title).write(filename)
         system.Visible = false unless visible
       end
+
+      #
+      # Returns the text of the active screen
+      #
+      # @return [String]
+      #
 
       def text
         rows = screen.Rows
