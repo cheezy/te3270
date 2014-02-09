b2f3d268a198f8aa46433738fbf186c34b03d30a
cheezy <jeff.morgan@leandog.com>
2014-02-09 07:27:03 -0500

    rdocs for populator

--- lib/te3270/screen_populator.rb
+++ lib/te3270/screen_populator.rb
@@ -2,10 +2,32 @@
 module TE3270
   module ScreenPopulator
 
+    #
+    # This method will populate all matched screen text fields from the
+    # Hash passed as an argument.  The way it find an element is by
+    # matching the Hash key to the name you provided when declaring
+    # the text field on your screen.
+    #
+    # @example
+    #   class ExampleScreen
+    #     include TE3270
+    #
+    #     text_field(:username, 1, 2, 20)
+    #   end
+    #
+    #   ...
+    #
+    #   @emulator = TE3270::emulator_for :quick3270
+    #   example_screen = ExampleScreen.new(@emulator)
+    #   example_screen.populate_screen_with :username => 'a name'
+    #
+    # @param [Hash] hsh the data to use to populate this screen.
+    #
     def populate_screen_with(hsh)
       hsh.each do |key, value|
         self.send("#{key}=", value) if self.respond_to? "#{key}=".to_sym
       end
     end
+
   end
 end
