da3bcec032bd241a2f6fb1b98b144af4b1d6ba7d
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2016-07-05 10:56:12 -0500

    Enhanced installation to produce messages and warnings.

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,17 +1,25 @@
-require 'rubygems/dependency_installer.rb'
+# ext/mkrf_conf.rb
 
+require 'rubygems/dependency_installer'
+
+puts 'Determining the correct dependencies to install'
 installer = Gem::DependencyInstaller.new
 begin
   if RUBY_PLATFORM == 'java'
+    puts "Installing jruby-win32ole because Ruby #{RUBY_PLATFORM}"
     installer.install "jruby-win32ole"
-  else 
+  else
+    puts "Installing win32screenshot because Ruby #{RUBY_PLATFORM}"
     installer.install "win32screenshot"
   end
 
-  rescue
-    exit(1)
+rescue => e
+  warn "#{$0}: #{e}"
+  exit!
 end  
 
-f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")
-f.write("task :default\n")
-f.close
+puts "Writing fake Rakefile"
+# Write fake Rakefile for rake since Makefile isn't used - indicates success
+File.open(File.join(File.dirname(__FILE__), 'Rakefile'), 'w') do |f|
+  f.write("task :default" + $/)
+end
